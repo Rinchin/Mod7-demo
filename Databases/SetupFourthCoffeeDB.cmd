1558b5f612506b583dcc2d8ae7d17e966e028dd0
Gomboev Rinchin <gomboev.rinchin@gmail.com>
2015-06-19 23:10:02 +0400

    Demo code: first using DateBase

---
+++ Databases/SetupFourthCoffeeDB.cmd
@@ -0,0 +1,3 @@
+sqlcmd -S (localdb)\v11.0 -i C:\microsoft_learning\Mod07\Democode\Databases\FourthCoffee.sql
+sqlcmd -S (localdb)\v11.0 -i C:\microsoft_learning\Mod07\Democode\Databases\FourthCoffee.sql
+
